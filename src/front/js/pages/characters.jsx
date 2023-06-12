218c78e2db9506bbacf52d61b821342b1c426001
robmab <56076087+robmab@users.noreply.github.com>
2023-06-12 16:03:55 +0000

    Fix

--- src/front/js/pages/characters.jsx
+++ src/front/js/pages/characters.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 
-import { Context } from "../../../front-back/js/store/appContext";
+import { Context } from "../store/appContext";
 
 import "../../styles/description.css";
 import { Description } from "../component/description.jsx";
